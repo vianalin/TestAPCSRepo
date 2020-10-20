e856489788ea3f826e47dbf52e7363a99003c3a2
vianalin <vlin21@stuy.edu>
2020-10-19 20:21:27 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo { 
+    public static void main(String[] args) {
+    	for(int i = 1; i <= 5; i++) {
+    		for(int j = 5; j >= i; j--) {
+    			System.out.print(i);
+    		}
+    		System.out.println();
+    	}
+    }
+}
+
