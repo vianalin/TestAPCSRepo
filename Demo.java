081a86feff66ad9530c43082602f3ec390992780
vianalin <vlin21@stuy.edu>
2020-10-22 14:35:50 -0400

    Deep array updated

---
+++ Demo.java
@@ -0,0 +1,35 @@
+public class Demo { 
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 5; j >= i; j--) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println(arrToString(new int[] {1}));
+        System.out.println(arrayDeepToString(new int[][] {{1}, {2,3}}));
+    }
+
+    public static String arrToString(int[] arr) {
+        String result = "{";
+        if(arr.length == 0) return "{}";
+        for(int i = 0; i < arr.length - 1; i++) {
+            result = result + arr[i] + ", ";
+        }
+        result = result + arr[arr.length - 1] + "}";
+        return result;
+    }
+
+    public static String arrayDeepToString(int[][] arr) {
+        String result = "{";
+        if(arr.length == 0) return result + "}";
+        for(int i = 0; i < arr.length - 1; i++) {
+            result = result + arrToString(arr[i]) + ", ";
+        }
+        result = result + arrToString(arr[arr.length - 1]) + "}";
+        return result;
+    }
+
+}
+
+
