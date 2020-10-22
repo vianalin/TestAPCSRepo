2954ee39b8de06453f3f7c27b801db58179cf02d
vianalin <vlin21@stuy.edu>
2020-10-22 14:14:46 -0400

    testing deep to string

--- Demo.java
+++ Demo.java
@@ -6,6 +6,32 @@ public class Demo {
     		}
     		System.out.println();
     	}
+    	System.out.println(arrToString(new int[] {1}));
+        System.out.println(arrDeepToString(new int[][] {{1}, {2,3}}));
     }
+
+    public static String arrToString(int[] arr) {
+    	String result = "{";
+    	if(arr.length == 0) return "{}";
+    	for(int i = 0; i < arr.length - 1; i++) {
+    		result = result + arr[i] + ", ";
+    	}
+    	result = result + arr[arr.length - 1] + "}";
+    	return result;
+    }
+
+    public static String arrayDeepToString(int[][] arr) {
+        String result = "{";
+        for(int i = 0; i < arr.length; i++) {
+            if(i < arr.length - 1) {
+                result += Integer.toString(arr[i]) + ", "; 
+            }
+            result += Integer.toString(arr[i]);
+        }
+        result += "}";
+        return result; 
+    }
+
+
 }
 
