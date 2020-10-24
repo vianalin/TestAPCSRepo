a7de26a74025cf1699eb88fb58c38e147eade8f1
vianalin <vlin21@stuy.edu>
2020-10-24 19:35:55 -0400

    commented out test case

--- Demo.java
+++ Demo.java
@@ -1,15 +1,15 @@
 public class Demo { 
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++) {
-            for(int j = 5; j >= i; j--) {
-                System.out.print(i);
-            }
-            System.out.println();
-        }
-        System.out.println(arrToString(new int[] {1}));
-        System.out.println(arrayDeepToString(new int[][] {{1}, {2,3}}));
-        System.out.println(arrayDeepToString(create2DArray(6, 4, 4)));
-        System.out.println(arrayDeepToString(create2DArrayRandomized(6, 4, 4)));
+        //for(int i = 1; i <= 5; i++) {
+            //for(int j = 5; j >= i; j--) {
+                //System.out.print(i);
+            //}
+            //System.out.println();
+        //}
+        //System.out.println(arrToString(new int[] {1}));
+        //System.out.println(arrayDeepToString(new int[][] {{1}, {2,3}}));
+        //System.out.println(arrayDeepToString(create2DArray(6, 4, 4)));
+        //System.out.println(arrayDeepToString(create2DArrayRandomized(6, 4, 4)));
     }
 
     public static String arrToString(int[] arr) {
